d160f11e2d367f71e90e35d97dc80b386bf401a7
mdsketch <draws.matthew@gmail.com>
2022-10-19 14:50:47 -0400

    more stuff

--- test.py
+++ test.py
@@ -21,21 +21,21 @@ async def main():
     Model = sw.ActiveDoc
     ARG_NULL = win32com.client.VARIANT(pythoncom.VT_DISPATCH, None)
     ck = Model.Extension.SelectByID2(
-        "Front", "PLANE", 0, 0, 0, False, 0, ARG_NULL, 0)
+        "Front Plane", "PLANE", 0, 0, 0, False, 0, ARG_NULL, 0)
     Model.SketchManager.InsertSketch(True)
     Model.ClearSelection2(True)
 
 # create 2 circles with a radius of 10 with the same center point
     Model.SketchManager.CreateCircleByRadius(0, 0, 0, 10)
     # put 2nd circle 10mm away from the first
-    Model.SketchManager.CreateCircleByRadius(10, 0, 3, 10)
+    Model.SketchManager.CreateCircleByRadius(0, 0, 0, 15)
     
 # extrude the the bottom circle 10mm in the Z direction
     # Model.ClearSelection2(True)
     # Model.Extension.SelectByID2("Circle1", "SKETCHSEGMENT", 0, 0, 0, False, 0, ARG_NULL, 0)
     # Model.Extension.SelectByID2("Circle2", "SKETCHSEGMENT", 0, 0, 0, True, 0, ARG_NULL, 0)
-    Model.FeatureManager.FeatureExtrusion3(True, False, False, 0, 0, 10, 10, False, False, False, False, 0, 0, False, False, False, False, True, True, True, 0, 0, 0)
-
+    #Model.FeatureManager.FeatureExtrusion3(True, False, False, 0, 0, 10, 10, False, False, False, False, 0, 0, False, False, False, False, True, True, True, 0, 0, 0)
+    Model.FeatureManager.FeatureExtrusion2(True, False, False, 0, 0, 50, 0, False, False, False, False, 0, 0, False, False, False, False, True, True, True, 0, 0, False)
 # create a cylinder with a radius of 10 and a height of 10 from 2 circles
 
 
